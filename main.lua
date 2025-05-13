4311b1ac0c620d55ae7dccfdf40ae6ff7d82b51e
NotTlaptop <128901254+Trong69@users.noreply.github.com>
2025-05-14 01:34:42 +0700

    add animation, jumping

--- main.lua
+++ main.lua
@@ -8,7 +8,9 @@ function love.load()
         sprites.playerSheet:getHeight())
 
     animations = {}
-    animations.
+    animations.idle = anim8.newAnimation(grid('1-15',1),0.045) 
+    animations.jump = anim8.newAnimation(grid('1-7',2),0.045)
+    animations.run = anim8.newAnimation(grid('1-15',3),0.045)
 
     wf = require 'libraries/windfield/windfield'
     world = wf.newWorld(0,1000,false)
@@ -18,12 +20,16 @@ function love.load()
     world:addCollisionClass('Player')
     world:addCollisionClass('Danger')
 
-
-
-    player = world:newRectangleCollider(360,100,80,80, {collision_class = "Player"})
+    --Player--
+    player = world:newRectangleCollider(360,100,40,100, {collision_class = "Player"})
     player:setFixedRotation(true)
     player.speed = 240
 
+    player.animation = animations.idle
+    player.isMoving = false
+    player.isJumping = false
+
+    -- platforms --
     platforms = world:newRectangleCollider(250,400,300,100, {collision_class = "Platform"})
     platforms:setType('static')
 
@@ -34,11 +40,15 @@ end
 function love.update(dt)
     world:update(dt)
     if player.body then
+        player.isMoving = false
+        --player.isJumping = false
         local px , py = player:getPosition()
         if love.keyboard.isDown('d') then
+            player.isMoving = true
             player:setX(px + player.speed*dt)
         end
         if love.keyboard.isDown('a') then
+            player.isMoving = true
             player:setX(px - player.speed*dt)
         end
 
@@ -46,28 +56,36 @@ function love.update(dt)
             player:destroy()
         end
     end
+    if player.isMoving == true then
+        player.animation = animations.run
+    elseif player.isJumping then
+        player.animation = animations.jump
+    else
+        player.animation = animations.idle
+    end
+    player.animation:update(dt)
 end
 
 function love.draw()
     world:draw()
+    local px, py = player:getPosition()
+    player.animation:draw(sprites.playerSheet,px,py,nil,0.25,nil,130,300)
 end
 
 function love.keypressed(key)
     if key == 'w' then
-        local colliders = world:queryRectangleArea(player:getX() - 40,
-            player:getY() + 40,80,2, {'Platform'})
+        local colliders = world:queryRectangleArea(player:getX() - 20,
+            player:getY() + 50,40,2, {'Platform'})
         -- number of collider is greater than 0 then jump 
-        if #colliders > 0 then          
-            player:applyLinearImpulse(0, -7000)
+        if #colliders > 0 then      
+            player.isJumping = true   
+            player:applyLinearImpulse(0, -4000)
         end
     end
 end
 
 function love.mousepressed(x,y, button)
     if button == 1 then
-        local colliders = world:queryCircleArea(x, y, 200)  
-        --for i, c in ipairs(colliders) do
-            --c:destroy()
-        --end
+        local colliders = world:queryCircleArea(x, y, 200)
     end
 end
